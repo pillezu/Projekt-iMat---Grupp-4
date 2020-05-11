78f550721f03b4388a67ccec60bec0bb25f34270
Oskar <ogg.developer@gmail.com>
2020-05-11 15:53:25 +0200

    Added files to filestructure

---
+++ iMat/src/sample/components/toolbar/Controller.java
@@ -0,0 +1,30 @@
+package sample.components.toolbar;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import sample.IMat;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    IMatDataHandler dataHandler = IMatDataHandler.getInstance();
+
+    @FXML Label iMatLabel;
+    @FXML ImageView homeImage;
+    @FXML VBox mainScreen;
+    @FXML Button btn1;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        btn1.setOnMouseClicked(mouseEvent -> {
+            IMat.changeRoot("History.fxml");
+        });
+    }
+}
