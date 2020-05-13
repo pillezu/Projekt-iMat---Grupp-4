d4a96e7d663e5c964353f4f2728c0f38906ee0a1
Fatemeh9312 <pegahamanzadeh@gmail.com>
2020-05-13 16:13:19 +0200

    checkout

--- iMat/src/sample/screens/checkout/Controller.java
+++ iMat/src/sample/screens/checkout/Controller.java
@@ -26,90 +26,13 @@ public class Controller implements Initializable {
     @FXML
     Button btn1;
 
-    @FXML
-    TextField nameTextField;
-    @FXML
-    TextField lastnameTextField;
-    @FXML
-    TextField epostTextField;
-    @FXML
-    TextField addressTextField;
-    @FXML
-    TextField postnrTextField;
-    @FXML
-    TextField telephonenrTextField;
-    @FXML
-    TextField accountTextField;
-    @FXML
-    TextField cvcTextField;
-    @FXML
-    Label kontouppgiftLabel;
-    @FXML
-    Label nameLabel;
-    @FXML
-    Label lastnameLabel;
-    @FXML
-    Label epostaddressLabel;
-    @FXML
-    Label addresslLabel;
-    @FXML
-    Label postnrLabel;
-    @FXML
-    Label telephonenrLabel;
-    @FXML
-    Label accountLabel;
-    @FXML
-    Label cvcLabel;
-    @FXML
-    Label validLabel;
-    @FXML
-    ComboBox monthComboBox;
-    @FXML
-    ComboBox yearComboBox;
-    @FXML
-    Button backButton;
-    @FXML
-    Button saveButton;
-
-
-    IMatDataHandler dataHandler = IMatDataHandler.getInstance();
-
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        nameTextField.focusedProperty().addListener(new TextFieldListener(nameTextField));
-        lastnameTextField.focusedProperty().addListener(new TextFieldListener(lastnameTextField));
-        epostTextField.focusedProperty().addListener(new TextFieldListener(epostTextField));
-        addressTextField.focusedProperty().addListener(new TextFieldListener(addressTextField));
-        postnrTextField.focusedProperty().addListener(new TextFieldListener(postnrTextField));
-        telephonenrTextField.focusedProperty().addListener(new TextFieldListener(telephonenrTextField));
-        accountTextField.focusedProperty().addListener(new TextFieldListener(accountTextField));
-        cvcTextField.focusedProperty().addListener(new TextFieldListener(cvcTextField));
 
-
-
-    }
-
-
-    private class TextFieldListener implements ChangeListener<Boolean> {
-        private TextField textField;
-
-        public TextFieldListener(TextField textField) {
-            this.textField = textField;
-        }
-
-        @Override
-        public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-            if (newValue) {
-                System.out.println();
-                System.out.println(textField);
-
-
-            }
-
-        }
     }
 }
+
 
 
 
