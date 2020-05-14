212559c1f7b8ada1e25560a5791241d4f3174ab9
Oskar <ogg.developer@gmail.com>
2020-05-14 11:06:39 +0200

    Merge branch 'master' of D:\code\Projekt-iMat---Grupp-4 with conflicts.

--- iMat/src/sample/screens/main/Controller.java
+++ iMat/src/sample/screens/main/Controller.java
@@ -2,8 +2,10 @@ package sample.screens.main;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.ProductCategory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,15 +13,25 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
     IMatDataHandler dataHandler = IMatDataHandler.getInstance();
+    ProductCategory[] categories = ProductCategory.values();
 
     @FXML
-    ListView categoriesListView;
+    ListView<String> categoriesListView;
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        for (int i = 0; i < 60; i++) {
-            categoriesListView.getItems().add("Beef");
+        for (int i = 0; i < categories.length; i++) {
+            ProductCategory category = categories[i];
+            String name = category.toString()
+                    .replace("_", " ");
+            name = name.substring(0, 1) + name.substring(1).toLowerCase();
+            categoriesListView.getItems().add(name);
         }
+        categoriesListView.getSelectionModel().selectedIndexProperty().addListener((observableValue, oldNumber, newNumber) -> {
+            int index = newNumber.intValue();
+            System.out.println("index: " + index);
+            System.out.println("category: " + categories[index]);
+        });
     }
 }
