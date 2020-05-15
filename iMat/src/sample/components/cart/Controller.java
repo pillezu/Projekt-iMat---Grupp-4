153c397c3da3a511fd704a9a5fc2c50e0df21c36
Oskar <ogg.developer@gmail.com>
2020-05-15 11:19:34 +0200

    Added items to cart

--- iMat/src/sample/components/cart/Controller.java
+++ iMat/src/sample/components/cart/Controller.java
@@ -1,8 +1,12 @@
 package sample.components.cart;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.FlowPane;
+import sample.components.cartitem.CartItem;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
 import se.chalmers.cse.dat216.project.ShoppingCart;
+import se.chalmers.cse.dat216.project.ShoppingItem;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,8 +16,18 @@ public class Controller implements Initializable {
     private IMatDataHandler dataHandler = IMatDataHandler.getInstance();
     private ShoppingCart cart = dataHandler.getShoppingCart();
 
+    @FXML
+    private FlowPane productsFlowPane;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        cart.addShoppingCartListener(cartEvent -> System.out.println(cart.getItems()));
+        cart.addShoppingCartListener(cartEvent -> {
+            System.out.println(cart.getItems());
+            productsFlowPane.getChildren().clear();
+
+            for (ShoppingItem item : cart.getItems()) {
+                productsFlowPane.getChildren().add(new CartItem(item.getProduct()));
+            }
+        });
     }
 }
