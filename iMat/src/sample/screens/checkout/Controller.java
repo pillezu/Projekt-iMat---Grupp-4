80e3044a2ef76208fc825101d29a2edfb698f194
Samdahl <Samdahl@protonmail.com>
2020-05-22 10:05:46 +0200

    checkout screen

--- iMat/src/sample/screens/checkout/Controller.java
+++ iMat/src/sample/screens/checkout/Controller.java
@@ -3,7 +3,11 @@ package sample.screens.checkout;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
 import sample.IMat;
+import sample.components.HorizontalProductItem.HorizontalProductItem;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.ShoppingItem;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,12 +16,18 @@ public class Controller implements Initializable {
 
     @FXML
     private Button toConfirmationButton;
+    @FXML
+    private FlowPane checkoutFlowPane;
+    private IMatDataHandler dataHandler = IMatDataHandler.getInstance();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        toConfirmationButton.setOnMouseClicked(mouseEvent -> IMat.changeRoot("screens/confirmation/Screen.fxml"));
+        for (ShoppingItem item : dataHandler.getShoppingCart().getItems()){
+            checkoutFlowPane.getChildren().add(new HorizontalProductItem(item));
+        }
 
+        toConfirmationButton.setOnMouseClicked(mouseEvent -> IMat.changeRoot("screens/confirmation/Screen.fxml"));
 
     }
 }
