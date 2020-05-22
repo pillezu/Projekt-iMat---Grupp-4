96307f7266c03b810959b594677e280f06c0c023
Samdahl <Samdahl@protonmail.com>
2020-05-22 11:23:34 +0200

    backend tidigare köp

--- iMat/src/sample/screens/history/Controller.java
+++ iMat/src/sample/screens/history/Controller.java
@@ -3,6 +3,7 @@ package sample.screens.history;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Accordion;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
@@ -17,10 +18,12 @@ public class Controller implements Initializable {
 
     private IMatDataHandler datahandler = IMatDataHandler.getInstance();
 
+    @FXML private Button clearOrdersButton;
     @FXML private VBox historyVBox;
     @FXML private Accordion historyAccordion;
     @FXML private FlowPane historyFlowPane;
     @FXML private ScrollPane historyScrollPane;
+    IMatDataHandler dataHandler = IMatDataHandler.getInstance();
     private static List<HistoryItem> historyItems = new ArrayList<>();  //innan vi avslutar och clearar ett köp lägg i ett nytt historyItem här med den shoppingcarten.
 
     @Override
@@ -28,16 +31,21 @@ public class Controller implements Initializable {
 
         historyScrollPane.setFitToWidth(true);
 
-        historyAccordion.getPanes().add(new HistoryItem(datahandler.getShoppingCart()));
+        for (Order order : dataHandler.getOrders()){
+            historyAccordion.getPanes().add(0, new HistoryItem(order));
+        }
+
+
         for (HistoryItem item : historyItems) {
             historyAccordion.getPanes().add(item);
         }
 
-        System.out.println(historyAccordion.getPanes().size());
+        clearOrdersButton.setOnMouseClicked(mouseEvent -> clearOrders());
     }
 
-    public static void addHistoryItem(ShoppingCart shoppingCart){
-        Controller.historyItems.add(0, new HistoryItem(shoppingCart));
+    private void clearOrders(){
+        dataHandler.getOrders().clear();
     }
+
 
 }
