b4c56a6d71f1a866029c8ede724ee1f3429bee20
Samdahl <Samdahl@protonmail.com>
2020-05-25 16:58:07 +0200

    checkout updates + checkoutProductItem fxml

---
+++ iMat/src/sample/components/checkoutProductItem/CheckoutProductItem.java
@@ -0,0 +1,68 @@
+package sample.components.checkoutProductItem;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import sample.components.AbstractProductItem;
+import se.chalmers.cse.dat216.project.*;
+
+import java.io.IOException;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
+
+public class CheckoutProductItem extends AbstractProductItem {
+
+
+    @FXML private Label totalPriceLabel;
+    @FXML private Label itemPriceLabel;
+
+    public CheckoutProductItem(ShoppingItem product){
+        super(product.getProduct());
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("CheckoutProductItem.fxml"));
+        fxmlLoader.setController(this);
+        fxmlLoader.setRoot(this);
+
+        try{
+            fxmlLoader.load();
+        }catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        setup();
+
+        setupShoppingCartListener();
+
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.CEILING);
+
+
+        totalPriceLabel.setText("Totalpris: " + df.format(product.getTotal()) + " kr");
+        itemPriceLabel.setText("Styckpris: " + df.format(product.getProduct().getPrice()) + " kr");
+    }
+
+    private void setupShoppingCartListener() {
+        dataHandler.getShoppingCart().addShoppingCartListener(cartEvent -> {
+            if (cartEvent.getShoppingItem().getProduct() == product) {
+                ShoppingItem item = getCartItemIfExists();
+                setNrProductsTextField(item);
+                setRemoveButtonDisabled(item);
+                updatePrices(item);
+            }
+        });
+    }
+
+    private void updatePrices(ShoppingItem item){
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.CEILING);
+
+        totalPriceLabel.setText("Totalpris: " + df.format(item.getTotal()) + " kr");
+        itemPriceLabel.setText("Styckpris: " + df.format(item.getProduct().getPrice()) + " kr");
+    }
+}
